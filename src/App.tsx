059c408afd0d634402980faebe830993a6067326
Nutt Chokwittaya <chokwittaya.nutt@gmail.com>
2022-10-22 14:35:16 -0500

    apply mui theme to our project

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,26 @@
+import { ThemeProvider } from '@emotion/react';
 import { BrowserRouter, Link } from 'react-router-dom';
 import Router, { URL_PATHS } from './pages/Router';
+import { theme } from './theme';
 
 function App() {
   const { HOME, LOGIN, NAME_CARDS } = URL_PATHS;
   return (
     <BrowserRouter>
-      <div className='App'>
-        <header>Thai Programmers in USA</header>
-        <nav>
-          <Link to={HOME}>Home</Link>
-          <br />
-          <Link to={LOGIN}>Login</Link>
-          <br />
-          <Link to={NAME_CARDS}>Name Cards</Link>
-        </nav>
-        <a href='#'>See Our Name Cards</a>
-      </div>
-      <Router />
+      <ThemeProvider theme={theme}>
+        <div className="App">
+          <header>Thai Programmers in USA</header>
+          <nav>
+            <Link to={HOME}>Home</Link>
+            <br />
+            <Link to={LOGIN}>Login</Link>
+            <br />
+            <Link to={NAME_CARDS}>Name Cards</Link>
+          </nav>
+          <a href="#">See Our Name Cards</a>
+        </div>
+        <Router />
+      </ThemeProvider>
     </BrowserRouter>
   );
 }
