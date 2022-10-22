bb06179fad5febd7b446823580e43a476d877987
BayLadyCoder <rachada_32@hotmail.com>
2022-10-21 21:17:31 -0400

    Fix render errors by creating placeholder components where the components are empty

---
+++ src/components/NameCard/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NameCard = () => {
+  return <div>NameCard</div>;
+};
+
+export default NameCard;
---
+++ src/pages/NameCards/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NameCards = () => {
+  return <div>NameCards</div>;
+};
+
+export default NameCards;
