8779de4155378a018599ce05d82911f4c275f1b4
Nutt Chokwittaya <chokwittaya.nutt@gmail.com>
2022-10-21 22:55:23 -0500

    ISSUE-6: [ Update - 4 ] - detach Routes to its own component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,5 @@
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
-import Home from './pages/Home';
-import Login from './pages/Login';
-import NameCards from './pages/NameCards';
-
-const URL_PATHS = {
-  HOME: '/',
-  LOGIN: 'login',
-  NAME_CARDS: 'name-cards',
-};
+import { BrowserRouter, Link } from 'react-router-dom';
+import Router, { URL_PATHS } from './pages/Router';
 
 function App() {
   const { HOME, LOGIN, NAME_CARDS } = URL_PATHS;
@@ -23,11 +15,7 @@ function App() {
           <Link to={NAME_CARDS}>Name Cards</Link>
         </nav>
       </div>
-      <Routes>
-        <Route path={HOME} element={<Home />} />
-        <Route path={LOGIN} element={<Login />} />
-        <Route path={NAME_CARDS} element={<NameCards />} />
-      </Routes>
+      <Router />
     </BrowserRouter>
   );
 }
