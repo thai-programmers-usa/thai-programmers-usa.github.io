0d1bf4245b7862bce703439fefd085d6c7dfd962
Nutt Chokwittaya <chokwittaya.nutt@gmail.com>
2022-10-21 21:32:47 -0500

    ISSUE-6: [ Update - 2 ] Add link and route for pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,25 @@
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import NameCards from './pages/NameCards';
+
 function App() {
   return (
-    <div className="App">
-      <header>Thai Programmers in USA</header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <header>Thai Programmers in USA</header>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="name-cards">Login</Link>
+          <Link to="name-cards">Name Cards</Link>
+        </nav>
+      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/" element={<NameCards />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
