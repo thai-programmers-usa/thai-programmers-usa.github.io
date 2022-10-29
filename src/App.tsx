63bacfbd13519cfa48f1a88b552d3297ba0035c0
Nutt Chokwittaya <65862558+CHNUTT@users.noreply.github.com>
2022-10-29 14:41:27 -0500

    Add draft navbar to our website (#29)
    * temp
    
    * add navbar and change the labels, links to our page
    
    * correct login wording
    
    * move login to far right

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
-import { ThemeProvider } from '@emotion/react';
 import { CssBaseline } from '@mui/material';
-import { BrowserRouter, Link } from 'react-router-dom';
-import Router, { URL_PATHS } from './pages/Router';
+import { BrowserRouter } from 'react-router-dom';
+import NavBar from './components/NavBar';
+import Router from './pages/Router';
+import { ThemeProvider } from '@mui/styles';
+
 import { theme } from './theme';
 
 function App() {
-  const { HOME, LOGIN, NAME_CARDS } = URL_PATHS;
+
   return (
     <BrowserRouter>
       <CssBaseline />
       <ThemeProvider theme={theme}>
-        <div className="App">
-          <header>Thai Programmers in USA</header>
-          <nav>
-            <Link to={HOME}>Home</Link>
-            <br />
-            <Link to={LOGIN}>Login</Link>
-            <br />
-            <Link to={NAME_CARDS}>Name Cards</Link>
-          </nav>
-          <a href="#">See Our Name Cards</a>
-        </div>
+        <NavBar />
         <Router />
       </ThemeProvider>
     </BrowserRouter>
--- src/pages/Router/index.tsx
+++ src/pages/Router/index.tsx
@@ -4,19 +4,30 @@ import Home from '../Home';
 import Login from '../Login';
 import NameCards from '../NameCards';
 
-export const URL_PATHS = {
-  HOME: '/',
-  LOGIN: 'login',
-  NAME_CARDS: 'name-cards',
-};
+export const URL_PATHS = [
+  {
+    label: 'HOME',
+    path: '/',
+    page: <Home />,
+  },
+  {
+    label: 'CARDS',
+    path: 'name-cards',
+    page: <NameCards />,
+  },
+  {
+    label: 'LOGIN',
+    path: 'login',
+    page: <Login />,
+  },
+];
 
 const Router = () => {
-  const { HOME, LOGIN, NAME_CARDS } = URL_PATHS;
   return (
     <Routes>
-      <Route path={HOME} element={<Home />} />
-      <Route path={LOGIN} element={<Login />} />
-      <Route path={NAME_CARDS} element={<NameCards />} />
+      {URL_PATHS.map(({ label, path, page }) => (
+        <Route key={label} path={path} element={page} />
+      ))}
     </Routes>
   );
 };
